815844e107735facc97f8eacfd8251fe01c1791d
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-04-02 16:30:42 -0300

    Adicionando gerador de componentes

---
+++ generators/plopfile.js
@@ -0,0 +1,39 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'Component',
+
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name?'
+      }
+    ],
+
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/components/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path:
+          '../src/components/{{pascalCase name}}/{{pascalCase name}}.styles.js',
+        templateFile: 'templates/components/styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path:
+          '../src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
+        templateFile: 'templates/components/test.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path:
+          '../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/components/stories.tsx.hbs'
+      }
+    ]
+  })
+}
